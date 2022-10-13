a85e976ab7003e4b3f6a4e9f1187f38e4d7e23b1
Raamishshah010 <raamishshah010@gmail.com>
2022-10-14 00:28:06 +0500

    Zeaco Hero Section

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// owl carousel init
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    // dots: true,
+    stagePadding: -5,
+    responsiveBaseElement: 'body',
+    responsive: {
+        0: {
+            items: 1,
+        },
+        700: {
+            items: 1,
+        },
+        1220: {
+            items: 1,
+        },
+    },
+});
